f6992a6f84b740c001ac164c13ef0760787fbff6
RuslanTs <tsygankov.r.v@gmail.com>
2020-01-05 20:33:11 +0300

    flask iris with get

--- hello.py
+++ hello.py
@@ -1,7 +1,48 @@
 from flask import Flask
+import numpy as np
+import pickle
+
+
 app = Flask(__name__)
+knn = pickle.load(open('model.pkl', "rb"))
 
 @app.route('/')
 def hello_world():
     print(1+12)
     return '<h1>Hello, Darling!!!</h1>'
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    username = float(username)*float(username)
+    return f'User {username}'
+
+def mean(numbers):
+    return float(sum(numbers))/max(len(numbers),1)
+    
+@app.route('/avg/<nums>')
+def avg(nums):
+    nums = nums.split(',')
+    nums = [float(num) for num in nums]
+    nums_mean = mean(nums)
+    print(nums_mean)
+    return str(nums_mean)
+
+@app.route('/iris/<params>')
+def iris(params):
+
+    params = params.split(',')
+    params = [float(num) for num in params]
+    params = np.array(params).reshape(1,-1)
+    predict = knn.predict(params)
+
+    return show_image(predict)
+
+@app.route('/show_image/<iris>')
+def show_image(iris):
+
+    setosa = '<img src="/static/setosa.jpg" alt="setosa">'
+    virginica = '<img src="/static/virginica.jpg" alt="virginica">'
+    versicolor = '<img src="/static/versicolor.jpg" alt="versicolor">'
+    images = {0: setosa, 1: versicolor, 2: virginica}
+    return images[int(iris)]
--- readme.md
+++ readme.md
@@ -1,1 +1,4 @@
 sudo docker exec -it flaskhello_flask_1 bash
+
+# train and save a model
+sudo docker exec -it flaskhello_flask_1 python train_model.py
